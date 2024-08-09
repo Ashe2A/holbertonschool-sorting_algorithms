1826ae0e0659aeb200a1f93d3ad83cd1c0cc2423
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-08-09 22:18:27 +0200

    1 - better upthought swap

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,27 +14,19 @@ void insertion_sort_list(listint_t **list)
 		current_node = current_node->next;
 		while (current_node->next != NULL)
 		{
-			if (current_node->n < current_node->prev->n)
+			if ((current_node->prev != NULL)
+			&& (current_node->n < current_node->prev->n))
 			{
 				tmp = current_node->prev;
-				if (current_node->prev->prev != NULL)
-				{
-					current_node->prev = current_node->prev->prev;
+				current_node->prev = current_node->prev->prev;
+				if (current_node->prev != NULL)
 					current_node->prev->next = current_node;
-				}
-				else
-					current_node->prev = NULL;
+				tmp->next = current_node->next;
 				if (current_node->next != NULL)
-				{
-					tmp->next = current_node->next;
-					current_node->next = tmp;
-				}
-				else
+					current_node->next->prev = tmp;
 				current_node->next = tmp;
 				tmp->prev = current_node;
-
 				print_list(list[0]);
-				current_node = current_node->prev;
 			}
 			else
 				current_node = current_node->next;
